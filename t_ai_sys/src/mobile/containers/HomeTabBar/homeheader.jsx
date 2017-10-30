73798ecad3de07094399858118d322fb13ac9d17
Jonathan Zhang <zhangjianan1996@qq.com>
2017-10-30 23:02:02 +0800

    update

--- t_ai_sys/src/mobile/containers/HomeTabBar/homeheader.jsx
+++ t_ai_sys/src/mobile/containers/HomeTabBar/homeheader.jsx
@@ -1,162 +1,159 @@
 import React from 'react'
-import {TabBar} from 'antd-mobile';
+import { Menu, ActivityIndicator, NavBar } from 'antd-mobile';
+import '../../static/css/App.css'
+
+const data = [
+    {
+        value: '1',
+        label: 'Food',
+        children: [
+            {
+                label: 'All Foods',
+                value: '1',
+                disabled: false,
+            },
+            {
+                label: 'Chinese Food',
+                value: '2',
+            }, {
+                label: 'Hot Pot',
+                value: '3',
+            }, {
+                label: 'Buffet',
+                value: '4',
+            }, {
+                label: 'Fast Food',
+                value: '5',
+            }, {
+                label: 'Snack',
+                value: '6',
+            }, {
+                label: 'Bread',
+                value: '7',
+            }, {
+                label: 'Fruit',
+                value: '8',
+            }, {
+                label: 'Noodle',
+                value: '9',
+            }, {
+                label: 'Leisure Food',
+                value: '10',
+            }],
+    }, {
+        value: '2',
+        label: 'Supermarket',
+        children: [
+            {
+                label: 'All Supermarkets',
+                value: '1',
+            }, {
+                label: 'Supermarket',
+                value: '2',
+                disabled: true,
+            }, {
+                label: 'C-Store',
+                value: '3',
+            }, {
+                label: 'Personal Care',
+                value: '4',
+            }],
+    },
+    {
+        value: '3',
+        label: 'Extra',
+        isLeaf: true,
+        children: [
+            {
+                label: 'you can not see',
+                value: '1',
+            },
+        ],
+    },
+];
 
 export default class HomeTabBar extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor(...args) {
+        super(...args);
         this.state = {
-            selectedTab: 'redTab',
-            hidden: false,
-            fullScreen: false,
+            initData: '',
+            show: false,
         };
-    }
+    };
 
-    renderContent(pageText) {
-        return (
-            <div style={{backgroundColor: 'white', height: '100%', textAlign: 'center'}}>
-                <div style={{paddingTop: 60}}>Clicked “{pageText}” tab， show “{pageText}” information</div>
-                <a style={{display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9'}}
-                   onClick={(e) => {
-                       e.preventDefault();
-                       this.setState({
-                           hidden: !this.state.hidden,
-                       });
-                   }}
-                >
-                    Click to show/hide tab-bar
-                </a>
-                <a style={{display: 'block', marginBottom: 600, color: '#108ee9'}}
-                   onClick={(e) => {
-                       e.preventDefault();
-                       this.setState({
-                           fullScreen: !this.state.fullScreen,
-                       });
-                   }}
-                >
-                    Click to switch fullscreen
-                </a>
-            </div>
-        );
-    }
+    onChange = (value) => {
+        let label = '';
+        data.forEach((dataItem) => {
+            if (dataItem.value === value[0]) {
+                label = dataItem.label;
+                if (dataItem.children && value[1]) {
+                    dataItem.children.forEach((cItem) => {
+                        if (cItem.value === value[1]) {
+                            label += ` ${cItem.label}`;
+                        }
+                    });
+                }
+            }
+        });
+        console.log(label);
+    };
+
+    handleClick = (e) => {
+        e.preventDefault(); // Fix event propagation on Android
+        this.setState({
+            show: !this.state.show,
+        });
+        // mock for async data loading
+        if (!this.state.initData) {
+            setTimeout(() => {
+                this.setState({
+                    initData: data,
+                });
+            }, 500);
+        }
+    };
+
+    onMaskClick = () => {
+        this.setState({
+            show: false,
+        });
+    };
 
     render() {
-        return (
-            <div style={{
-                position: 'fixed',
-                height: '100%',
-                width: '100%',
-                top: 0
-            }}>
-                <TabBar
-                    unselectedTintColor="#949494"
-                    tintColor="#33A3F4"
-                    barTintColor="white"
-                    hidden={this.state.hidden}
-                >
-                    <TabBar.Item
-                        title="Life"
-                        key="Life"
-                        icon={<div style={{
-                            width: '22px',
-                            height: '22px',
-                            background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat'
-                        }}
-                        />
-                        }
-                        selectedIcon={<div style={{
-                            width: '22px',
-                            height: '22px',
-                            background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat'
-                        }}
-                        />
-                        }
-                        selected={this.state.selectedTab === 'blueTab'}
-                        badge={1}
-                        onPress={() => {
-                            this.setState({
-                                selectedTab: 'blueTab',
-                            });
-                        }}
-                        data-seed="logId"
-                    >
-                        {this.renderContent('Life')}
-                    </TabBar.Item>
-                    <TabBar.Item
-                        icon={
-                            <div style={{
-                                width: '22px',
-                                height: '22px',
-                                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat'
-                            }}
-                            />
-                        }
-                        selectedIcon={
-                            <div style={{
-                                width: '22px',
-                                height: '22px',
-                                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat'
-                            }}
-                            />
-                        }
-                        title="Koubei"
-                        key="Koubei"
-                        badge={'new'}
-                        selected={this.state.selectedTab === 'redTab'}
-                        onPress={() => {
-                            this.setState({
-                                selectedTab: 'redTab',
-                            });
-                        }}
-                        data-seed="logId1"
-                    >
-                        {this.renderContent('Koubei')}
-                    </TabBar.Item>
-                    <TabBar.Item
-                        icon={
-                            <div style={{
-                                width: '22px',
-                                height: '22px',
-                                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat'
-                            }}
-                            />
-                        }
-                        selectedIcon={
-                            <div style={{
-                                width: '22px',
-                                height: '22px',
-                                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat'
-                            }}
-                            />
-                        }
-                        title="Friend"
-                        key="Friend"
-                        dot
-                        selected={this.state.selectedTab === 'greenTab'}
-                        onPress={() => {
-                            this.setState({
-                                selectedTab: 'greenTab',
-                            });
-                        }}
-                    >
-                        {this.renderContent('Friend')}
-                    </TabBar.Item>
-                    <TabBar.Item
-                        icon={{uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg'}}
-                        selectedIcon={{uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg'}}
-                        title="My"
-                        key="my"
-                        selected={this.state.selectedTab === 'yellowTab'}
-                        onPress={() => {
-                            this.setState({
-                                selectedTab: 'yellowTab',
-                            });
-                        }}
-                    >
-                        {this.renderContent('My')}
-                    </TabBar.Item>
-                </TabBar>
+        const { initData, show } = this.state;
+        const menuEl = (
+            <Menu
+                className="foo-menu"
+                data={initData}
+                value={['1', '3']}
+                onChange={this.onChange}
+                height={document.documentElement.clientHeight * 0.6}
+            />
+        );
+        const loadingEl = (
+            <div style={{ width: '100%', height: document.documentElement.clientHeight * 0.6, display: 'flex', justifyContent: 'center' }}>
+                <ActivityIndicator size="large" />
             </div>
         );
-    }
+        return (
+            <div className={show ? 'menu-active' : ''}>
+                <div>
+                    <NavBar
+                        leftContent="Menu"
+                        mode="light"
+                        icon={<img style={{width:'15px'}} src="https://gw.alipayobjects.com/zos/rmsportal/iXVHARNNlmdCGnwWxQPH.svg" className="am-icon am-icon-md" alt="" />}
+                        onLeftClick={this.handleClick}
+                        className="top-nav-bar"
+                    >
+                        {this.props.pageText}
+                    </NavBar>
+                </div>
+                {show ? initData ? menuEl : loadingEl : null}
+                {show ? <div className="menu-mask" onClick={this.onMaskClick} /> : null}
+
+            </div>
+        );
+    };
+
 }
 
