fdaa49092fc325e18e5939907bcffb277c75bf36
Jonathan Zhang <zhangjianan1996@qq.com>
2017-11-03 13:18:01 +0800

    update

--- t_ai_sys/src/index.js
+++ t_ai_sys/src/index.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
+import reducers from './reducer'
+import './config'
+import MediaQuery from 'react-responsive';
 import MobileApp from './mobile/App'
 import PCApp from './pc/App'
-import MediaQuery from 'react-responsive';
 
-class Root extends React.Component{
-    render(){
-        return(
-            <div>
-                <MediaQuery query='(max-device-width:1224px)'>
-                    <MobileApp/>
-                </MediaQuery>
-                <MediaQuery query='(min-device-width:1224px)'>
-                    <PCApp/>
-                </MediaQuery>
-            </div>
+const store = createStore(reducers, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+class Root extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <div>
+                    <MediaQuery query='(max-device-width:1224px)'>
+                        <BrowserRouter>
+                            <MobileApp/>
+                        </BrowserRouter>
+                    </MediaQuery>
+                    <MediaQuery query='(min-device-width:1224px)'>
+                        <BrowserRouter>
+                            <PCApp/>
+                        </BrowserRouter>
+                    </MediaQuery>
+                </div>
+            </Provider>
         );
     };
 }
 
-ReactDom.render(<Root/>,document.getElementById('root'));
+ReactDom.render(<Root/>, document.getElementById('root'));
