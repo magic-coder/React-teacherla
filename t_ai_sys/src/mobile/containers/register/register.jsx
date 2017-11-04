af921190e6f7fb9768fdf19e778e8613b05cd054
Jonathan Zhang <zhangjianan1996@qq.com>
2017-11-04 12:08:44 +0800

    update

---
+++ t_ai_sys/src/mobile/containers/register/register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Register extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>登录页面</h2>
+            </div>
+        )
+    }
+}
+
+export default Register
