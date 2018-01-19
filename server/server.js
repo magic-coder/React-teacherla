33480dfdfd427df295ed2e830eff0b43a60d7444
Jonathan Zhang <zhangjianan1996@qq.com>
2018-01-19 14:59:17 +0800

    update

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const userRouter = require('./user')
-const app = express()
-//操作
-app.use('/user',userRouter)
-//
-app.listen(9093,function () {
-   console.log('Node app start at port 9093');
-})
