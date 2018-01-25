37509247811934ff75b8105c29dc6ad0cc89d30a
Jonathan Zhang <zhangjianan1996@qq.com>
2018-01-25 15:43:03 +0800

    update new build

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">123</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
