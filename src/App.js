ff8f71bc9d6d6c067c90c8993a33b076c77405e6
Jonathan Zhang <zhangjianan1996@qq.com>
2018-02-20 23:19:59 +0800

    update store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import Loading from './component/Loading/Loading'
 import IndexRouter from './router/IndexRouter'
+import configStore from './redux/store'
+
+const store = configStore()
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <Provider store={store}>
         <Loading>
           <IndexRouter/>
         </Loading>
-      </div>)
+      </Provider>)
   }
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import reducers from './reducers/reducers.redux'
 
 export default function configStore() {
