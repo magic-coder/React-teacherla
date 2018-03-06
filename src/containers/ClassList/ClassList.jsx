d8675850d620d7d83d04a39faea17f57df222183
Jonathan Zhang <zhangjianan1996@qq.com>
2018-03-06 14:51:32 +0800

    update classlist visitlist

--- src/containers/ClassList/ClassList.jsx
+++ src/containers/ClassList/ClassList.jsx
@@ -1,11 +1,66 @@
 import React from 'react'
-import { Calendar } from 'antd'
+import {DatePicker, Card, Avatar} from 'antd';
+import moment from 'moment';
 
-class ClassList extends React.PureComponent {
+const dateFormat = 'MM月DD日';
+const { Meta } = Card;
+const todayTime = moment(new Date()).format(dateFormat);
+
+class ClassList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      dateTime: moment(new Date()).format(dateFormat)
+    }
+    this.dateChange = this.dateChange.bind(this)
+  }
+  dateChange(v, str) {
+    this.setState({dateTime: str})
+  }
   render() {
+    const disabledDate = function (current) {
+      return  current  <= (new Date()).getTime()-1000*60*60*24;
+    };
     return (<div>
-        <Calendar fullscreen={false} />
-      </div>)
+      <h2>
+        <span style={{
+            color: '#1890ff'
+          }}>{this.state.dateTime === todayTime ? '今天' : this.state.dateTime}</span>您要上的课程
+      </h2>
+      <DatePicker disabledDate={disabledDate} defaultValue={moment()} format={dateFormat} onChange={this.dateChange}/>
+      <Card style={{
+          marginTop: 20,
+          width: '100%'
+        }}>
+        <Meta avatar={<Avatar src = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="软件工程" description={<div > <p>上午第三节课</p>
+          <p>任课老师：陈维斌</p>
+        </div>}/>
+      </Card>
+      <Card style={{
+          marginTop: 20,
+          width: '100%'
+        }}>
+        <Meta avatar={<Avatar src = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="软件工程" description={<div > <p>上午第三节课</p>
+          <p>任课老师：陈维斌</p>
+        </div>}/>
+      </Card>
+      <Card style={{
+          marginTop: 20,
+          width: '100%'
+        }}>
+        <Meta avatar={<Avatar src = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="软件工程" description={<div > <p>上午第三节课</p>
+          <p>任课老师：陈维斌</p>
+        </div>}/>
+      </Card>
+      <Card style={{
+          marginTop: 20,
+          width: '100%'
+        }}>
+        <Meta avatar={<Avatar src = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="软件工程" description={<div > <p>上午第三节课</p>
+          <p>任课老师：陈维斌</p>
+        </div>}/>
+      </Card>
+    </div>)
   }
 }
 
--- src/containers/VisitList/VisitList.jsx
+++ src/containers/VisitList/VisitList.jsx
@@ -1,8 +1,67 @@
 import React from 'react'
+import {DatePicker, Card, Avatar} from 'antd';
+import moment from 'moment';
+
+const dateFormat = 'MM月DD日';
+const { Meta } = Card;
+const todayTime = moment(new Date()).format(dateFormat);
+
 
 class VisitList extends React.PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      dateTime: moment(new Date()).format(dateFormat),
+    }
+    this.dateChange = this.dateChange.bind(this)
+  }
+  dateChange(v, str) {
+    this.setState({dateTime: str})
+  }
   render() {
-    return (<h1>听课列表</h1>)
+    const disabledDate = function (current) {
+      return  current  <= (new Date()).getTime()-1000*60*60*24;
+    };
+    return (<div>
+      <h2>
+        <span style={{
+            color: '#1890ff'
+          }}>{this.state.dateTime === todayTime ? '今天' : this.state.dateTime}</span>可以听的课
+      </h2>
+      <DatePicker disabledDate={disabledDate} defaultValue={moment()} format={dateFormat} onChange={this.dateChange}/>
+      <Card style={{
+          marginTop: 20,
+          width: '100%'
+        }}>
+        <Meta avatar={<Avatar src = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="软件工程" description={<div > <p>上午第三节课</p>
+          <p>任课老师：陈维斌</p>
+        </div>}/>
+      </Card>
+      <Card style={{
+          marginTop: 20,
+          width: '100%'
+        }}>
+        <Meta avatar={<Avatar src = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="软件工程" description={<div > <p>上午第三节课</p>
+          <p>任课老师：陈维斌</p>
+        </div>}/>
+      </Card>
+      <Card style={{
+          marginTop: 20,
+          width: '100%'
+        }}>
+        <Meta avatar={<Avatar src = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="软件工程" description={<div > <p>上午第三节课</p>
+          <p>任课老师：陈维斌</p>
+        </div>}/>
+      </Card>
+      <Card style={{
+          marginTop: 20,
+          width: '100%'
+        }}>
+        <Meta avatar={<Avatar src = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title="软件工程" description={<div > <p>上午第三节课</p>
+          <p>任课老师：陈维斌</p>
+        </div>}/>
+      </Card>
+    </div>)
   }
 }
 
