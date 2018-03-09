7ee6c2fe651895593d81c50a0a0d416c20fc4e1d
TYRMars <zhangjianan1996@qq.com>
2018-03-10 01:20:43 +0800

    update user

--- server/user.js
+++ server/user.js
@@ -5,53 +5,60 @@ const User = model.getModel('user')
 const Check = model.getModel('check')
 
 //用户列表
-Router.get('/list',function (req,res) {
-  User.find({},function (err,doc) {
+Router.get('/list', function(req, res) {
+  //User.remove({}, function(e,d) {})
+  User.find({}, function(err, doc) {
     return res.json(doc)
   })
 })
 
 //生成验证码
-Router.post('/addinvitnum',function (req,res) {
-  const {user,invitnum} = {
-    user:'1403102048',
-    'invitnum':'202083'
-  }
-  Check.create({user,invitnum},function (err,doc) {
-    console.log(doc);
-  })
-})
+// Router.get('/addinvitnum',function (req,res) {
+//   const {user,invitnum} = {
+//     user:'1403102043',
+//     invitnum:'234567'
+//   }
+//   Check.create({user,invitnum},function (err,doc) {
+//     if(err){
+//       return res.json({code:1,msg:'后端出错'});
+//     }
+//     return res.json({code:0})
+//   })
+// })
 
 //验证信息列表
-Router.get('/invitnum',function (req,res) {
-  Check.find({},function (err,doc) {
+Router.get('/invitnum', function(req, res) {
+  Check.find({}, function(err, doc) {
     return res.json(doc)
   })
 })
 
 //注册
-Router.post('/register',function (req,res) {
+Router.post('/register', function(req, res) {
   console.log(req.body)
   const {user, password, invitnum} = req.body
-  Check.findOne({user:user,invitnum:invitenum},function (err,doc) {
+  User.findOne({user: user}, function(err, doc) {
     if (doc) {
-      User.findOne({user:user},function (err,doc) {
-        User.create({user, password},function (e,d) {
-          if (e) {
-            return res.json({code:1,msg:'后端出错了'})
-          }
-          return res.json({code:0})
-        })
+      return res.json({code: 1, msg: '用户已经注册'})
+    } else {
+      Check.findOne({user: user,invitnum: invitnum}, function(err, doc) {
+        if (doc) {
+          return res.json({code: 1, msg: '邀请码错误'})
+        } else {
+          User.create({user,password}, function(err, doc) {
+            if (err) {
+              return res.json({code: 1, msg: '后端出错了'})
+            }
+            return res.json({code: 0})
+          })
+        }
       })
-    }else {
-      return res.json({code:1,msg:'邀请码错误'})
-    }
-  })
+  }})
 })
 
-//
-Router.get('/info',function (req, res) {
-  return res.json({code:0})
+//用户信息
+Router.get('/info', function(req, res) {
+  return res.json({code: 1})
 })
 
 module.exports = Router
--- src/router/indexRouter.jsx
+++ src/router/indexRouter.jsx
@@ -3,12 +3,13 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import HomeRouter from './HomeRouter'
 import Login from '../containers/Login/Login'
 import Register from '../containers/Register/Register'
-//import AuthRouter from '../component/AuthRouter/AuthRouter'
+import AuthRouter from '../component/AuthRouter/AuthRouter'
 
 class IndexRouter extends React.PureComponent {
   render() {
     return (<BrowserRouter>
       <div>
+        <AuthRouter/>
         <Switch>
           <Route path='/login' component={Login}/>
           <Route path='/register' component={Register}/>
