a668b738fc0c8d6ff208424393c4c195a21b96c4
TYRMars <zhangjianan1996@qq.com>
2018-03-21 00:13:25 +0800

    update ClassStep

---
+++ src/component/ClassReadyStep/ClassReadyStep.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Input } from 'antd';
+
+class ClassReadyStep extends React.Component {
+  render(){
+    return (<div>
+
+      </div>)
+  }
+}
+
+export default ClassReadyStep
--- src/containers/ClassStep/ClassStep.jsx
+++ src/containers/ClassStep/ClassStep.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
-import { Steps, Button, message } from 'antd';
+import {Steps, Button, message} from 'antd';
+
+import ClassReadyStep from '../../component/ClassReadyStep/ClassReadyStep'
+
 import './classstep.css'
 const Step = Steps.Step;
 
-const steps = [{
-  title: '课程准备',
-  content: 'First-content',
-}, {
-  title: '听课阶段',
-  content: 'Second-content',
-}, {
-  title: '课程评价',
-  content: 'Last-content',
-}];
+const steps = [
+  {
+    title: '课程准备'
+  }, {
+    title: '听课阶段'
+  }, {
+    title: '课程评价'
+  }
+];
 
 class ClassStep extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      current: 0,
+      current: 0
     };
     this.next = this.next.bind(this)
     this.prev = this.prev.bind(this)
+    this.content = this.content.bind(this)
   }
   next() {
     const current = this.state.current + 1;
@@ -31,16 +34,35 @@ class ClassStep extends React.Component {
     const current = this.state.current - 1;
     this.setState({current});
   }
+  content() {
+    switch (this.state.current) {
+      case 0:
+        return (<ClassReadyStep/>)
+      case 1:
+        return (<div>1</div>)
+      case 2:
+        return (<ClassReadyStep/>)
+      default:
+        return null
+    }
+  }
   render() {
-    const { current } = this.state;
+    const {current} = this.state;
     return (<div>
       <Steps size="small" current={current}>
         {steps.map(item => <Step key={item.title} title={item.title}/>)}
       </Steps>
-      <div className="steps-content">{steps[this.state.current].content}</div>
+      <div className="steps-content">
+        {
+          this.content()
+        }
+      </div>
       <div className="steps-action">
         {this.state.current < steps.length - 1 && <Button type="primary" onClick={() => this.next()}>下一步</Button>}
-        {this.state.current === steps.length - 1 && <Button type="primary" onClick={() => message.success('完成听课!')}> 完成</Button>}
+        {
+          this.state.current === steps.length - 1 && <Button type="primary" onClick={() => message.success('完成听课!')}>
+              完成</Button>
+        }
         {
           this.state.current > 0 && <Button style={{
                 marginLeft: 8
