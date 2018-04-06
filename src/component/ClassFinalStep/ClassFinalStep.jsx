839a8ab3ae8eb0ece6fb73926f30a50bbf8849ef
TYRMars <zhangjianan1996@qq.com>
2018-04-06 21:44:00 +0800

    update finalClass

--- src/component/ClassFinalStep/ClassFinalStep.jsx
+++ src/component/ClassFinalStep/ClassFinalStep.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Slider, InputNumber, Row, Col, Tabs } from 'antd';
+const TabPane = Tabs.TabPane;
 
 class ClassFinalStep extends React.Component {
+  state = {
+    inputValue: 1,
+  }
+  onChange = (value) => {
+    this.setState({
+      inputValue: value,
+    });
+  }
   render(){
     return (<div>
-       结束组件
+       <h3>教学评价质量表</h3>
+       <Tabs>
+         <TabPane tab="教师情况" key="table-1">
+            <Row>
+             <Col span={12}>
+               <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
+             </Col>
+             <Col span={4}>
+               <InputNumber
+                 min={1}
+                 max={20}
+                 style={{ marginLeft: 16 }}
+                 value={this.state.inputValue}
+                 onChange={this.onChange}
+               />
+             </Col>
+           </Row>
+         </TabPane>
+         <TabPane tab="学生情况" key="table-2">
+         </TabPane>
+       </Tabs>
       </div>)
   }
 }
