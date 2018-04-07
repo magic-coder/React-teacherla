608c18649841bc269c1275e8c8d0acb048c0d0d9
TYRMars <zhangjianan1996@qq.com>
2018-04-07 23:24:13 +0800

    update Ready

--- src/component/ClassFinalStep/ClassFinalStep.jsx
+++ src/component/ClassFinalStep/ClassFinalStep.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Slider, InputNumber, Row, Col, Tabs } from 'antd';
+import { Slider, InputNumber, Row, Col, Tabs, Divider } from 'antd';
 const TabPane = Tabs.TabPane;
 
 class ClassFinalStep extends React.Component {
@@ -16,11 +16,13 @@ class ClassFinalStep extends React.Component {
        <h3>教学评价质量表</h3>
        <Tabs>
          <TabPane tab="教师情况" key="table-1">
+            <p>备课充分，有关教学资料准备符合要求和规范</p>
             <Row>
+             <Col span={2} />
              <Col span={12}>
                <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
              </Col>
-             <Col span={4}>
+             <Col span={2}>
                <InputNumber
                  min={1}
                  max={20}
@@ -30,6 +32,8 @@ class ClassFinalStep extends React.Component {
                />
              </Col>
            </Row>
+           <p>精神饱满，讲课有激情，有感染力</p>
+           <p>板书规范，语言准确、表达生动</p>
          </TabPane>
          <TabPane tab="学生情况" key="table-2">
          </TabPane>
--- src/component/ClassReadyStep/ClassReadyStep.jsx
+++ src/component/ClassReadyStep/ClassReadyStep.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon, Input } from 'antd';
+import { Input } from 'antd';
 
 
 class ClassReadyStep extends React.Component {
