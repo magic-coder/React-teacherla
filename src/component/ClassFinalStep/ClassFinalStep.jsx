d14d181091e84b2f1931d12754ff2cb2db98ec76
TYRMars <zhangjianan1996@qq.com>
2018-04-12 00:12:34 +0800

    update final

--- src/component/ClassFinalStep/ClassFinalStep.jsx
+++ src/component/ClassFinalStep/ClassFinalStep.jsx
@@ -33,14 +33,149 @@ class ClassFinalStep extends React.Component {
              </Col>
            </Row>
            <p>精神饱满，讲课有激情，有感染力</p>
+           <Row>
+            <Col span={2} />
+            <Col span={12}>
+              <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
+            </Col>
+            <Col span={2}>
+              <InputNumber
+                min={1}
+                max={20}
+                style={{ marginLeft: 16 }}
+                value={this.state.inputValue}
+                onChange={this.onChange}
+              />
+            </Col>
+          </Row>
            <p>板书规范，语言准确、表达生动，或能使用多媒体教学手段、图像清效果好、不是简单的文字堆积</p>
+           <Row>
+            <Col span={2} />
+            <Col span={12}>
+              <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
+            </Col>
+            <Col span={2}>
+              <InputNumber
+                min={1}
+                max={20}
+                style={{ marginLeft: 16 }}
+                value={this.state.inputValue}
+                onChange={this.onChange}
+              />
+            </Col>
+          </Row>
            <p>基本概念表达简练准确、思路清晰、重点难点突出</p>
+           <Row>
+            <Col span={2} />
+            <Col span={12}>
+              <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
+            </Col>
+            <Col span={2}>
+              <InputNumber
+                min={1}
+                max={20}
+                style={{ marginLeft: 16 }}
+                value={this.state.inputValue}
+                onChange={this.onChange}
+              />
+            </Col>
+          </Row>
            <p>对课程内容娴熟，运用自如</p>
+           <Row>
+            <Col span={2} />
+            <Col span={12}>
+              <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
+            </Col>
+            <Col span={2}>
+              <InputNumber
+                min={1}
+                max={20}
+                style={{ marginLeft: 16 }}
+                value={this.state.inputValue}
+                onChange={this.onChange}
+              />
+            </Col>
+          </Row>
            <p>讲述内容充实、信息量大</p>
+           <Row>
+            <Col span={2} />
+            <Col span={12}>
+              <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
+            </Col>
+            <Col span={2}>
+              <InputNumber
+                min={1}
+                max={20}
+                style={{ marginLeft: 16 }}
+                value={this.state.inputValue}
+                onChange={this.onChange}
+              />
+            </Col>
+          </Row>
            <p>教学内容能反应或联系学科发展的新思想、新概念、新成果</p>
+           <Row>
+            <Col span={2} />
+            <Col span={12}>
+              <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
+            </Col>
+            <Col span={2}>
+              <InputNumber
+                min={1}
+                max={20}
+                style={{ marginLeft: 16 }}
+                value={this.state.inputValue}
+                onChange={this.onChange}
+              />
+            </Col>
+          </Row>
            <p>注意启发学生思考和联想，给学生以创新的启迪</p>
+           <Row>
+            <Col span={2} />
+            <Col span={12}>
+              <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
+            </Col>
+            <Col span={2}>
+              <InputNumber
+                min={1}
+                max={20}
+                style={{ marginLeft: 16 }}
+                value={this.state.inputValue}
+                onChange={this.onChange}
+              />
+            </Col>
+          </Row>
            <p>能有效调控课堂秩序，并通过适当互动，调动学生学习情绪</p>
+           <Row>
+            <Col span={2} />
+            <Col span={12}>
+              <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
+            </Col>
+            <Col span={2}>
+              <InputNumber
+                min={1}
+                max={20}
+                style={{ marginLeft: 16 }}
+                value={this.state.inputValue}
+                onChange={this.onChange}
+              />
+            </Col>
+          </Row>
            <p>结合学生的理解水平组织教学，教学调度张弛有度</p>
+           <Row>
+            <Col span={2} />
+            <Col span={12}>
+              <Slider min={1} max={10} onChange={this.onChange} value={this.state.inputValue} />
+            </Col>
+            <Col span={2}>
+              <InputNumber
+                min={1}
+                max={20}
+                style={{ marginLeft: 16 }}
+                value={this.state.inputValue}
+                onChange={this.onChange}
+              />
+            </Col>
+          </Row>
          </TabPane>
          <TabPane tab="学生情况" key="table-2">
             <p>遵守制度、按时到课、出勤率高</p>
