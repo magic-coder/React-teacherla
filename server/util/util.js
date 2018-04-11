199d9ca71d4bed57b356b6127b7febf1d129bcf5
Jonathan Zhang <zhangjianan1996@qq.com>
2018-04-11 23:00:58 +0800

    update server

--- server/util/util.js
+++
@@ -1,8 +0,0 @@
-const utils = require('utility')
-
-const md5Pwd = function (pwd) {
-  const salt = 'XIT_Tla_teacher_pwd_$xitjisuanji#'
-  return utils.md5(utils.md5(pwd+salt))
-};
-
-module.exports = md5Pwd;
