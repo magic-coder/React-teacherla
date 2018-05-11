e1f2a32e23e101be48b8931117ed46a9c7dbe842
TYRMars <zhangjianan1996@qq.com>
2018-05-11 14:54:41 +0800

    update

--- src/component/Msg/Msg.jsx
+++ src/component/Msg/Msg.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { messages } from ''
+import { Breadcrumb } from 'antd';
 
 class extends React.component {
   render(){
     return(
       <div>
-
+        <Breadcrumb>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item><a href="">Application Center</a></Breadcrumb.Item>
+          <Breadcrumb.Item><a href="">Application List</a></Breadcrumb.Item>
+          <Breadcrumb.Item>An Application</Breadcrumb.Item>
+        </Breadcrumb>
       </div>
     )
   }
