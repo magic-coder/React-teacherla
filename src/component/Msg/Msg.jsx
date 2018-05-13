541d17abc839090c4d34d899f37defef9867ba19
TYRMars <zhangjianan1996@qq.com>
2018-05-13 09:45:55 +0800

    update

--- src/component/Msg/Msg.jsx
+++ src/component/Msg/Msg.jsx
@@ -18,10 +18,6 @@ class extends React.component {
           style={{ width: 240 }}
           cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
         >
-          <Meta
-            title="Europe Street beat"
-            description="www.instagram.com"
-          />
         </Card>
       </div>
     )
