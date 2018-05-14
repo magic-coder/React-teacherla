6aba19daf2efc8a707bc7c7e58fac038762f9c48
TYRMars <zhangjianan1996@qq.com>
2018-05-15 00:02:24 +0800

    update

--- src/component/Msg/Msg.jsx
+++ src/component/Msg/Msg.jsx
@@ -7,12 +7,7 @@ class extends React.component {
   render(){
     return(
       <div>
-        <Breadcrumb>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item><a href="">Application Center</a></Breadcrumb.Item>
-          <Breadcrumb.Item><a href="">Application List</a></Breadcrumb.Item>
-          <Breadcrumb.Item>An Application</Breadcrumb.Item>
-        </Breadcrumb>
+
       </div>
     )
   }
