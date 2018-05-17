ed1d2739a97e04b4775cfd5d26c0e64e12d681d1
TYRMars <zhangjianan1996@qq.com>
2018-05-17 14:47:54 +0800

    update

--- src/component/Msg/Msg.jsx
+++ src/component/Msg/Msg.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from 'react'
+import {Carousel} from 'antd'
+import './carousel.css'
 
 class Msg extends React.component {
   render(){
-    return(
-      <div>
-
-      </div>
-    )
+    return(<Carousel autoplay="autoplay">
+        <div>
+          <img src="http://www.xit.edu.cn/xygh/UploadFiles_5553/200905/2009050723025005.jpg" alt=""/>
+        </div>
+        <div>
+          <img src="http://www.xit.edu.cn/xygh/UploadFiles_5553/200905/2009050708423373.jpg" alt=""/>
+        </div>
+        <div>
+          <img src="http://www.xit.edu.cn/xygh/UploadFiles_5553/200905/2009050621384227.jpg" alt=""/>
+        </div>
+        <div>
+          <img src="http://www.xit.edu.cn/xygh/UploadFiles_5553/200905/2009050717135659.jpg" alt=""/>
+        </div>
+      </Carousel>)
   }
 }
 
