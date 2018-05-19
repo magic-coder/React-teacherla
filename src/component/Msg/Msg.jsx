87229aca839dd945a5c93a46c9ba1655e6ef0b6e
TYRMars <zhangjianan1996@qq.com>
2018-05-19 23:27:01 +0800

    update

--- src/component/Msg/Msg.jsx
+++ src/component/Msg/Msg.jsx
@@ -5,12 +5,6 @@ import './carousel.css'
 class Msg extends React.component {
   render(){
     return(<Carousel autoplay="autoplay">
-        <div>
-          <img src="http://www.xit.edu.cn/xygh/UploadFiles_5553/200905/2009050723025005.jpg" alt=""/>
-        </div>
-        <div>
-          <img src="http://www.xit.edu.cn/xygh/UploadFiles_5553/200905/2009050708423373.jpg" alt=""/>
-        </div>
       </Carousel>)
   }
 }
